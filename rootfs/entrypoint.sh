dcbdac0ada54b9e4941ce130ab050ed789ba14ae
Tatsuya Kitagawa <tatsuya.kitagawa@t13a.com>
2018-10-15 22:04:45 +0900

    Change user `bcmove` -> `root`

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -2,16 +2,9 @@
 
 set -euo pipefail
 
-BCMOVE_USER=bcmove
-BCMOVE_GROUP=bcmove
-BCMOVE_HOME=/bcmove
-
-addgroup -S ${BCMOVE_GROUP}
-adduser -h ${BCMOVE_HOME} -g ${BCMOVE_GROUP} -s /sbin/nologin -G ${BCMOVE_GROUP} -SDH ${BCMOVE_USER}
-
 if [ -z "${CROND_DISABLED:-}" ]
 then
-    cat << EOF > /var/spool/cron/crontabs/${BCMOVE_USER}
+    cat << EOF > /var/spool/cron/crontabs/root
 ${CROND_SCHEDULE} /usr/local/bin/bcmove cron
 EOF
 fi
